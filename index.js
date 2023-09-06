10d7c1d82dca93b254672fb17f43aaf7a4a06e6d
tsmit275 <taylor.lsmith03@gmail.com$ git config --global user.taylor.lsmith03@gmail.com>
2023-09-06 14:32:15 -0400

    new

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(left, bottom, src){
+let characters = document.createElement('img')
+characters.src = src
+characters.style.position = 'fixed'
+characters.style.left = left +"px"
+characters.style.bottom = bottom +"px"
+document.body.append(characters)
+return characters
+}
+newImage(450,200,'assets/pine-tree.png')
+newImage(100,100,'assets/green-character.gif')
+newImage(200,300,'assets/tree.png')
+newImage(350,100,'assets/pillar.png')
+newImage(150,200,'assets/crate.png')
+newImage(500,425,'assets/well.png')
+
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
+}
+
+newItem(500,405,'assets/sword.png')
+newItem(165,185,'assets/shield.png')
+newItem(600,100,'assets/staff.png')
